133f3d040a946339eb064039fe571da17948a86c
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-04-12 22:30:38 -0500

    Update index.js
    Required 'http' and 'url'

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const http = require("http"),
+  url = require("url");
+
+http
+  .createServer((request, response) => {
+    let requestURL = url.parse(request.url, ture);
+    if (requestURL.pathname == "/documentation.html") {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end("Documentation on the martial arts movies API.\n");
+    } else {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+    }
+  })
+  .listen(8080);
+
+console.log("My first Node test server is running on port 8080.");
