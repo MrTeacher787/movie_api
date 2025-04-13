cd9d9caeb864c5a1e2835bffb66e7536d4668bf7
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-04-12 23:03:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const express = require("express");
-const app = express();
-const http = require("http"),
-  url = require("url");
+const http = require('http');
 
-http
-  .createServer((request, response) => {
-    let requestURL = url.parse(request.url, ture);
-    if (requestURL.pathname == "/documentation.html") {
-      response.writeHead(200, { "Content-Type": "text/plain" });
-      response.end("Documentation on the martial arts movies API.\n");
-    } else {
-      response.writeHead(200, { "Content-Type": "text/plain" });
-    }
-  })
-  .listen(8080);
+http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Welcome to my book club!\n');
+}).listen(8080);
 
-console.log("My first Node test server is running on port 8080.");
+console.log('My first Node test server is running on Port 8080.');
