9ced976d5074dc2c5abba6741b867d8db92512d1
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2025-05-31 22:34:50 -0500

    Update passport.js

--- passport.js
+++ passport.js
@@ -14,24 +14,28 @@ passport.use(
         passwordField: 'Password',
     },
     async (username, password, callback) => {
-        console.log(`${username} ${password}`);
-        await Users.findOne({ Username: username })
-        .then((user) => {
-            if (!user) {
-              console.log('incorrect username');
-              return callback(null, false, {
-                message: 'Incorrect username or password.',
-              });
-            }
-            console.log('finished');
-            return callback(null, user);
-        })
-        .catch((error) => {
-          if (error) {
-            console.log(error);
-            return callback(error);
-          }
-        })
+      console.log(`${username} ${password}`);
+      await Users.findOne({ Username: username })
+      .then((user) => {
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, {
+            message: 'Incorrect username or password.',
+          });
+        }
+        if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, { message: 'Incorrect Password,' });
+        }
+        console.log('finished');
+        return callback(null, user);
+      })
+      .catch((error) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+      })
     }
   )  
 );
